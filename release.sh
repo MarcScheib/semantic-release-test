f5b16f638ae8e170b5cce353eda347a104c62cbd
Marc Scheib <marc.scheib@gmx.de>
2024-03-22 20:25:42 +0100

    ci: add workflow

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx --yes -p @semantic-release/changelog -p @semantic-release/git semantic-release --dry-run
+npx --yes -p @semantic-release/changelog -p @semantic-release/git semantic-release --dry-run --no-ci
